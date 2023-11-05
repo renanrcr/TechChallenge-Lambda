a6655c1a02153d86d1bf8f15331f95fba12346ba
Renan de Carvalho Rinaldi <renan.crinaldi@gmail.com>
2023-11-05 16:58:01 -0300

    feat: removendo classes não utilizadas.

--- src/TechChallenge.Api/Controllers/AuthClienteController.cs
+++ src/TechChallenge.Api/Controllers/AuthClienteController.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using TechChallenge.Api.Controllers.Base;
 using TechChallenge.Api.Domain.Adapters;
@@ -10,18 +9,12 @@ namespace TechChallenge.Api.Controllers
     internal class AuthClienteController : BaseController
     {
         private readonly IMediator _mediator;
-        private readonly IMapper _mapper;
-        private readonly IAutenticaClienteRepository _autenticaClienteRepository;
 
         public AuthClienteController(INotificador notificador,
-            IMediator mediator,
-            IMapper mapper,
-            IAutenticaClienteRepository autenticaClienteRepository)
+            IMediator mediator)
             : base(notificador)
         {
             _mediator = mediator;
-            _mapper = mapper;
-            _autenticaClienteRepository = autenticaClienteRepository;
         }
 
         [HttpPost]
